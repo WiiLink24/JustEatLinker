ca85e7a571cbdb5787143e597657b23f9674bfb3
Harry Walker <contact@harrywalker.uk>
2026-04-11 11:17:35 +0100

    feat: update translations

--- translations/translation_de.ts
+++ translations/translation_de.ts
@@ -4,12 +4,12 @@
 <context>
     <name>CountrySelect</name>
     <message>
-        <location filename="../just_eat.py" line="41"/>
+        <location filename="../just_eat.py" line="39"/>
         <source>Select your country</source>
         <translation>Wähle dein Land</translation>
     </message>
     <message>
-        <location filename="../just_eat.py" line="43"/>
+        <location filename="../just_eat.py" line="42"/>
         <source>Select the country associated with your Just Eat Account.</source>
         <translatorcomment>here in germany &quot;Just Eat&quot; means &quot;Lieferando&quot; https://de.wikipedia.org/wiki/Lieferando</translatorcomment>
         <translation>Wähle das Land aus, das mit deinem Lieferando-Konto verknüft ist.</translation>
@@ -18,12 +18,12 @@
 <context>
     <name>FinalPage</name>
     <message>
-        <location filename="../JustEatLinker.py" line="100"/>
+        <location filename="../JustEatLinker.py" line="97"/>
         <source>Linking success!</source>
         <translation>Verknüpfung erfolgreich!</translation>
     </message>
     <message>
-        <location filename="../JustEatLinker.py" line="102"/>
+        <location filename="../JustEatLinker.py" line="100"/>
         <source>Your Just Eat account has been linked to your WiiLink account!</source>
         <translation>Dein Lieferando-Konto wurde mit deinem WiiLink-Konto verknüpft!</translation>
     </message>
@@ -40,17 +40,17 @@ Viel Spaß beim Essenbestellen auf deiner Wii!</translation>
 <context>
     <name>IntroPage</name>
     <message>
-        <location filename="../JustEatLinker.py" line="67"/>
+        <location filename="../JustEatLinker.py" line="68"/>
         <source>Welcome to the WiiLink Just Eat Linker!</source>
         <translation>Willkommen beim WiiLink-Lieferando-Linker!</translation>
     </message>
     <message>
-        <location filename="../JustEatLinker.py" line="70"/>
+        <location filename="../JustEatLinker.py" line="73"/>
         <source>This tool will assist you in linking your WiiLink Account with Just Eat.</source>
         <translation>Dieses Tool hilft dir dabei, dein WiiLink-Konto mit Lieferando zu verknüpfen.</translation>
     </message>
     <message>
-        <location filename="../JustEatLinker.py" line="76"/>
+        <location filename="../JustEatLinker.py" line="79"/>
         <source>Welcome to the WiiLink Just Eat Linker!
 
 With this tool, you&apos;ll be able to link your WiiLink Account with Just Eat for use with the Demae Channel.
@@ -64,90 +64,63 @@ Drücke auf „Weiter“, um zu starten!</translation>
     </message>
 </context>
 <context>
-    <name>JustEat2FAPage</name>
-    <message>
-        <location filename="../just_eat.py" line="339"/>
-        <source>Just Eat Login</source>
-        <translation>Lieferando-Anmeldung</translation>
-    </message>
-    <message>
-        <location filename="../just_eat.py" line="340"/>
-        <source>Enter the 2FA code sent to your Just Eat email</source>
-        <translation>Gib den 2FA-Code ein, der an deine Lieferando-E-Mail gesendet wurde</translation>
-    </message>
-    <message>
-        <location filename="../just_eat.py" line="342"/>
-        <source>Code:</source>
-        <translation>Code:</translation>
-    </message>
-    <message>
-        <location filename="../just_eat.py" line="344"/>
-        <source>Code...</source>
-        <translation>Code...</translation>
-    </message>
-    <message>
-        <location filename="../just_eat.py" line="346"/>
-        <source>Login</source>
-        <translation>Anmelden</translation>
-    </message>
-</context>
-<context>
     <name>JustEatCredentialsPage</name>
     <message>
-        <location filename="../just_eat.py" line="87"/>
+        <location filename="../just_eat.py" line="85"/>
         <source>Just Eat Login</source>
         <translation>Lieferando-Anmeldung</translation>
     </message>
     <message>
-        <location filename="../just_eat.py" line="88"/>
+        <location filename="../just_eat.py" line="86"/>
         <source>Enter your credentials for your Just Eat Account.</source>
         <translation>Gib deine Anmeldedaten für dein Lieferando-Konto ein.</translation>
     </message>
     <message>
-        <location filename="../just_eat.py" line="90"/>
-        <source>Username:</source>
-        <translation>Benutzername:</translation>
-    </message>
-    <message>
-        <location filename="../just_eat.py" line="92"/>
-        <source>Username...</source>
-        <translation>Benutzername...</translation>
-    </message>
-    <message>
-        <location filename="../just_eat.py" line="94"/>
-        <source>Password:</source>
-        <translation>Passwort:</translation>
-    </message>
-    <message>
         <location filename="../just_eat.py" line="96"/>
-        <source>Password...</source>
-        <translation>Passwort...</translation>
-    </message>
-    <message>
-        <location filename="../just_eat.py" line="99"/>
-        <source>Reset Password...</source>
-        <translation>Passwort zurücksetzen…</translation>
-    </message>
-    <message>
-        <location filename="../just_eat.py" line="108"/>
-        <source>Login</source>
-        <translation>Anmelden</translation>
+        <source>In the browser that opens, login to your Just Eat account.
+
+It will automatically close once login is completed.
+
+IMPORTANT - DO NOT RESIZE THE BROWSER WINDOW. DOING SO CAN CAUSE THE PROCESS TO FAIL.</source>
+        <translation>Im sich öffnenden Browser melde dich bei deinem Lieferando-Konto an.
+
+Er schließt sich automatisch, sobald die Anmeldung abgeschlossen ist.
+
+WICHTIG - ÄNDERE NICHT DIE GRÖSSE DES BROWSERFENSTERS. ANDERNFALLS KANN DER VORGANG FEHLSCHLAGEN.</translation>
     </message>
 </context>
 <context>
     <name>JustEatLinker</name>
     <message>
-        <location filename="../JustEatLinker.py" line="167"/>
+        <location filename="../JustEatLinker.py" line="170"/>
+        <source>Browser not found</source>
+        <translation>Browser nicht gefunden</translation>
+    </message>
+    <message>
+        <location filename="../JustEatLinker.py" line="177"/>
+        <source>To use this app, you need to have Chromium, Google Chrome, or Microsoft Edge installed.
+
+If you are on Linux, the browser also needs to be accessible on the system PATH.
+
+If you are using Linux and do not wish to install a browser as a system package, you can download and extract a browser, then pass the full path to this app with the environment variable `WIILINK_BROWSER_PATH`.</source>
+        <translation>Um diese App zu verwenden, musst du Chromium, Google Chrome oder Microsoft Edge installiert haben.
+
+Wenn du Linux verwendest, muss der Browser außerdem über den System-PATH erreichbar sein.
+
+Falls du unter Linux keinen Browser als Systempaket installieren möchtest, kannst du einen Browser herunterladen und entpacken und dann den vollständigen Pfad zu diesem Browser über die Umgebungsvariable `WIILINK_BROWSER_PATH` an diese App übergeben.</translation>
+    </message>
+    <message>
+        <location filename="../JustEatLinker.py" line="183"/>
         <source>WiiLink Just Eat Linker</source>
         <translation>WiiLink Lieferando-Linker</translation>
     </message>
     <message>
-        <location filename="../JustEatLinker.py" line="174"/>
+        <location filename="../JustEatLinker.py" line="190"/>
         <source>Next</source>
         <translation>Weiter</translation>
     </message>
     <message>
-        <location filename="../JustEatLinker.py" line="175"/>
+        <location filename="../JustEatLinker.py" line="191"/>
         <source>Back</source>
         <translation>Zurück</translation>
     </message>
@@ -155,12 +128,12 @@ Drücke auf „Weiter“, um zu starten!</translation>
 <context>
     <name>WiiLinkAccountPage</name>
     <message>
-        <location filename="../oauth.py" line="57"/>
+        <location filename="../oauth.py" line="56"/>
         <source>Login to your WiiLink Account</source>
         <translation>Melde dich bei deinem WiiLink-Konto an</translation>
     </message>
     <message>
-        <location filename="../oauth.py" line="58"/>
+        <location filename="../oauth.py" line="57"/>
         <source>Login with your browser</source>
         <translation>Mit deinem Browser anmelden</translation>
     </message>
@@ -170,7 +143,7 @@ Drücke auf „Weiter“, um zu starten!</translation>
         <translation>Besuche &lt;a href=&apos;{}?code={}&apos;&gt;{}&lt;/a&gt; und gib den folgenden Code ein:&lt;/br&gt;&lt;/br&gt;</translation>
     </message>
     <message>
-        <location filename="../oauth.py" line="74"/>
+        <location filename="../oauth.py" line="76"/>
         <source>Upon success you will be redirected to the next page.</source>
         <translation>Bei Erfolg wirst du zur nächsten Seite weitergeleitet.</translation>
     </message>
@@ -178,17 +151,17 @@ Drücke auf „Weiter“, um zu starten!</translation>
 <context>
     <name>WiiNumberSelector</name>
     <message>
-        <location filename="../oauth.py" line="190"/>
+        <location filename="../oauth.py" line="191"/>
         <source>Select your Wii Number</source>
         <translation>Wähle deine Wii-Nummer aus</translation>
     </message>
     <message>
-        <location filename="../oauth.py" line="192"/>
+        <location filename="../oauth.py" line="194"/>
         <source>Select the Wii number you want to link with Just Eat.</source>
         <translation>Wähle die Wii-Nummer aus, die du mit Lieferando verknüpfen möchtest.</translation>
     </message>
     <message>
-        <location filename="../oauth.py" line="239"/>
+        <location filename="../oauth.py" line="243"/>
         <source>Currently, you have no Wiis linked to your account.&lt;br&gt;&lt;br&gt;
 
 Follow the guide at &lt;a href=&apos;https://wiilink.ca/guide/accounts&apos;&gt;https://wiilink.ca/guide/accounts&lt;/a&gt; to link your console.&lt;br&gt;&lt;br&gt;
--- translations/translation_it.ts
+++ translations/translation_it.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es_ES">
+<TS version="2.1" language="it_IT">
 <context>
     <name>CountrySelect</name>
     <message>
         <location filename="../just_eat.py" line="39"/>
         <source>Select your country</source>
-        <translation>Selecciona tu país</translation>
+        <translation>Seleziona il tuo paese</translation>
     </message>
     <message>
         <location filename="../just_eat.py" line="42"/>
         <source>Select the country associated with your Just Eat Account.</source>
-        <translation>Selecciona el país asociado con tu cuenta de Just Eat.</translation>
+        <translation>Seleziona il paese associato al tuo account Just Eat.</translation>
     </message>
 </context>
 <context>
@@ -19,21 +19,21 @@
     <message>
         <location filename="../JustEatLinker.py" line="97"/>
         <source>Linking success!</source>
-        <translation>¡Vinculación completada!</translation>
+        <translation>Collegamento riuscito!</translation>
     </message>
     <message>
         <location filename="../JustEatLinker.py" line="100"/>
         <source>Your Just Eat account has been linked to your WiiLink account!</source>
-        <translation>¡Tu cuenta de Just Eat se ha vinculado con tu cuenta de WiiLink!</translation>
+        <translation>Il tuo account Just Eat è stato collegato al tuo account WiiLink!</translation>
     </message>
     <message>
         <location filename="../JustEatLinker.py" line="106"/>
         <source>Your Just Eat account has successfully been linked to the Wii with number &lt;strong&gt;{}&lt;/strong&gt;.&lt;/br&gt;&lt;/br&gt;
 
 Enjoy ordering food on your Wii!</source>
-        <translation>Tu cuenta de Just Eat se ha vinculado satisfactoriamente con la Wii con el número &lt;strong&gt;{}&lt;/strong&gt;.&lt;/br&gt;&lt;/br&gt;
+        <translation>Il tuo account Just Eat è stato collegato correttamente alla Wii con il numero &lt;strong&gt;{}&lt;/strong&gt;.&lt;/br&gt;&lt;/br&gt;
 
-¡Disfruta pidiendo comida desde tu Wii!</translation>
+Divertiti a ordinare cibo sulla tua Wii!</translation>
     </message>
 </context>
 <context>
@@ -41,12 +41,12 @@ Enjoy ordering food on your Wii!</source>
     <message>
         <location filename="../JustEatLinker.py" line="68"/>
         <source>Welcome to the WiiLink Just Eat Linker!</source>
-        <translation>¡Te damos la bienvenida al vinculador de Just Eat con WiiLink!</translation>
+        <translation>Benvenuto/a nel linker WiiLink Just Eat!</translation>
     </message>
     <message>
         <location filename="../JustEatLinker.py" line="73"/>
         <source>This tool will assist you in linking your WiiLink Account with Just Eat.</source>
-        <translation>Esta herramienta te ayudará a vincular tu cuenta de WiiLink con Just Eat.</translation>
+        <translation>Questo strumento ti aiuterà a collegare il tuo account WiiLink a Just Eat.</translation>
     </message>
     <message>
         <location filename="../JustEatLinker.py" line="79"/>
@@ -55,11 +55,12 @@ Enjoy ordering food on your Wii!</source>
 With this tool, you&apos;ll be able to link your WiiLink Account with Just Eat for use with the Demae Channel.
 
 Press &apos;Next&apos; to get started!</source>
-        <translation>¡Te damos la bienvenida al vinculador de Just Eat con WiiLink!
+        <translation>Benvenuto/a nel linker WiiLink Just Eat!
+		
+Con questo strumento, potrai collegare il tuo account WiiLink a Just Eat per utilizzare il canale Demae.
 
-Con esta herramienta, podrás vincular tu cuenta de WiiLink con Just Eat para poder usarla en el Canal Comida.
-
-¡Pulsa “Siguiente” para empezar!</translation>
+Premi &apos;Avanti&apos; per iniziare!
+		</translation>
     </message>
 </context>
 <context>
@@ -67,12 +68,12 @@ Con esta herramienta, podrás vincular tu cuenta de WiiLink con Just Eat para po
     <message>
         <location filename="../just_eat.py" line="85"/>
         <source>Just Eat Login</source>
-        <translation>Iniciar sesión en Just Eat</translation>
+        <translation>Just Eat Login</translation>
     </message>
     <message>
         <location filename="../just_eat.py" line="86"/>
         <source>Enter your credentials for your Just Eat Account.</source>
-        <translation>Introduce las credenciales de tu cuenta de Just Eat.</translation>
+        <translation>Inserire credenziali del tuo account Just Eat.</translation>
     </message>
     <message>
         <location filename="../just_eat.py" line="96"/>
@@ -103,17 +104,17 @@ If you are using Linux and do not wish to install a browser as a system package,
     <message>
         <location filename="../JustEatLinker.py" line="183"/>
         <source>WiiLink Just Eat Linker</source>
-        <translation>Vinculador de Just Eat con WiiLink</translation>
+        <translation>WiiLink Just Eat Linker</translation>
     </message>
     <message>
         <location filename="../JustEatLinker.py" line="190"/>
         <source>Next</source>
-        <translation>Siguiente</translation>
+        <translation>Avanti</translation>
     </message>
     <message>
         <location filename="../JustEatLinker.py" line="191"/>
         <source>Back</source>
-        <translation>Volver</translation>
+        <translation>Indietro</translation>
     </message>
 </context>
 <context>
@@ -121,22 +122,22 @@ If you are using Linux and do not wish to install a browser as a system package,
     <message>
         <location filename="../oauth.py" line="56"/>
         <source>Login to your WiiLink Account</source>
-        <translation>Iniciar sesión en tu cuenta de WiiLink</translation>
+        <translation>Accedi al tuo account WiiLink</translation>
     </message>
     <message>
         <location filename="../oauth.py" line="57"/>
         <source>Login with your browser</source>
-        <translation>Iniciar sesión con el navegador</translation>
+        <translation>Accedi con il tuo browser</translation>
     </message>
     <message>
         <location filename="../oauth.py" line="62"/>
         <source>Visit &lt;a href=&apos;{}?code={}&apos;&gt;{}&lt;/a&gt; and enter the code below:&lt;/br&gt;&lt;/br&gt;</source>
-        <translation>Visita &lt;a href=&apos;{}?code={}&apos;&gt;{}&lt;/a&gt; e introduce el siguiente código:&lt;/br&gt;&lt;/br&gt;</translation>
+        <translation>Visita &lt;a href=&apos;{}?code={}&apos;&gt;{}&lt;/a&gt; e inserisci il codice qui sotto:&lt;/br&gt;&lt;/br&gt;</translation>
     </message>
     <message>
         <location filename="../oauth.py" line="76"/>
         <source>Upon success you will be redirected to the next page.</source>
-        <translation>Cuando lo hagas, se te redirigirá a la siguiente página.</translation>
+        <translation>In caso di successo, verrai reindirizzato alla pagina successiva.</translation>
     </message>
 </context>
 <context>
@@ -144,12 +145,12 @@ If you are using Linux and do not wish to install a browser as a system package,
     <message>
         <location filename="../oauth.py" line="191"/>
         <source>Select your Wii Number</source>
-        <translation>Selecciona tu número de Wii</translation>
+        <translation>Seleziona il tuo numero Wii</translation>
     </message>
     <message>
         <location filename="../oauth.py" line="194"/>
         <source>Select the Wii number you want to link with Just Eat.</source>
-        <translation>Selecciona el número de Wii que quieras vincular con Just Eat.</translation>
+        <translation>Seleziona il numero della console Wii che desideri collegare a Just Eat.</translation>
     </message>
     <message>
         <location filename="../oauth.py" line="243"/>
@@ -158,11 +159,11 @@ If you are using Linux and do not wish to install a browser as a system package,
 Follow the guide at &lt;a href=&apos;https://wiilink.ca/guide/accounts&apos;&gt;https://wiilink.ca/guide/accounts&lt;/a&gt; to link your console.&lt;br&gt;&lt;br&gt;
 
 Then, run this app again.</source>
-        <translation>Actualmente, no tienes ninguna Wii vinculada a tu cuenta.&lt;br&gt;&lt;br&gt;
+        <translation>Al momento, non hai nessuna console Wii collegata al tuo account. &lt;br&gt;&lt;br&gt;
 
-Sigue la guía en &lt;a href=&apos;https://wiilink.ca/guide/accounts&apos;&gt;https://wiilink.ca/guide/accounts&lt;/a&gt; para vincular tu consola.&lt;br&gt;&lt;br&gt;
+Segui la guida su &lt;a href=&apos;https://wiilink.ca/guide/accounts&apos;&gt;https://wiilink.ca/guide/accounts&lt;/a&gt; per collegare la tua console.&lt;br&gt;&lt;br&gt;
 
-Después, ejecuta esta aplicación de nuevo.</translation>
+Dopodiché, esegui nuovamente l&apos;applicazione.</translation>
     </message>
 </context>
 </TS>
